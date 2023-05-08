92807f9fc8fdb0dd3b959768180cd704efe13971
Kaity Hayes <kaitynhayes@gmail.com>
2023-05-08 15:29:33 -0500

    set up

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a small team of developers building awesome React apps!</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ portfolio/src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>You can reach us at react@example.com</p>
+    </div>
+  );
+}
+
+export default Contact;
